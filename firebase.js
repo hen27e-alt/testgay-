4336f3faab45ba74ad9c558460c53f17e39cdc94
hen27e-alt <hen27e@gmail.com>
2026-02-20 21:15:02 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,8 +1,12 @@
 const firebaseConfig = {
-apiKey: "YOUR_API_KEY",
-authDomain: "gays-triv.firebaseapp.com",
-projectId: "gays-triv"
+  apiKey: "AIzaSyCdYTrrgk1FYw42xmAaNGltcAOEYyLC8Mw",
+  authDomain: "gays-triv.firebaseapp.com",
+  databaseURL: "https://gays-triv-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "gays-triv",
+  storageBucket: "gays-triv.firebasestorage.app",
+  messagingSenderId: "408293564501",
+  appId: "1:408293564501:web:843c48a52fed57ed3bf4dc",
+  measurementId: "G-YV9X1T6SFF"
 };
 
 firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
